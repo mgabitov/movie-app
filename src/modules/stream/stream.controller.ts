3da8e0b65780c6ad71aec61a9caf94f99cd06eab
unknown <2links@gmail.com>
2022-12-02 01:19:07 -0800

    Added webtorrent package, modified stream controller

--- src/modules/stream/stream.controller.ts
+++ src/modules/stream/stream.controller.ts
@@ -1,11 +1,33 @@
 import { Router } from 'express'
+import WebTorrent from 'webtorrent'
 
 const router = Router()
+const client = new WebTorrent()
 
-router.get('/', (req, res) => {
-    res.status(200).send({
-        hello: 'World'
-    })
+let state = {
+    progress: 0,
+    downloadSpeed: 0,
+    ratio: 0
+}
+
+let error
+
+client.on('error', (err: Error) => {
+    console.error('err', err.message)
+    error = err.message
+})
+
+client.on('torrent', () => {
+    console.log(client.progress)
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+})
+
+router.get('/stats', (req, res) => {
+    res.status(200).send(state)
 })
 
 export default router
