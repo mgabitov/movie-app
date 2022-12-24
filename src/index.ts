ef756f0f223c9fae7529e62b66922412862ac738
unknown <2links@gmail.com>
2022-12-24 07:54:50 -0800

    First stream is working

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
-
+import * as path from 'path'
 
 import streamRouter from './modules/stream/stream.controller'
+import contentRouter from './modules/content/content.controller'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+app.set('views', path.join(__dirname, 'views'))
 
 app.use('/stream', streamRouter)
+app.use('/content', contentRouter)
 
 const PORT = process.env.PORT || 8080
 
--- src/modules/stream/stream.controller.ts
+++ src/modules/stream/stream.controller.ts
@@ -1,8 +1,9 @@
-import { Router } from 'express'
-import WebTorrent from 'webtorrent'
+import { Router, Response, Request, NextFunction } from 'express'
+import WebTorrent, { Torrent, TorrentFile} from 'webtorrent'
 
 const router = Router()
 const client = new WebTorrent()
+
 
 let state = {
     progress: 0,
@@ -26,8 +27,96 @@ client.on('torrent', () => {
     }
 })
 
+
+router.get('/add/:magnet', (req, res) => {
+    const magnet = req.params.magnet
+    const torrentId = 'magnet:?xt=urn:btih:bd4120b2e28481e551746cb77f159548933e5f5a&dn=RUTOR.ORG_%D0%9F%D0%BE%D1%82%D1%80%D1%8F%D1%81%D0%B0%D1%8E%D1%89%D0%B5%D0%B5+%D0%A0%D0%BE%D0%B6%D0%B4%D0%B5%D1%81%D1%82%D0%B2%D0%BE+/+A+Holiday+Spectacular+(2022)+WEB-DL+1080p+%D0%BE%D1%82+ExKinoRay+|+Jaskier&tr=udp://opentor.net:6969&tr=http://retracker.local/announce'
+    console.log(torrentId)
+    client.add(torrentId, torrent => {
+        const files = torrent.files.map(data => ({
+            name: data.name,
+            length: data.length
+        }))
+        console.log(files)
+ 
+        res.status(200).send(files)
+    })
+})
+
 router.get('/stats', (req, res) => {
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
     res.status(200).send(state)
 })
+
+interface StreamRequest extends Request {
+    params: {
+        magnet: string,
+        fileName: string 
+    }
+    headers: {
+        range: string
+    }
+}
+
+interface ErrorWithStatus extends Error {
+    status: number
+}
+
+router.get('/:magnet/:fileName', (req: StreamRequest, res: Response, next: NextFunction) => {
+    const {
+        params: {magnet, fileName},
+        headers: {range}
+    } = req
+
+    if (!range) {
+        const err = new Error('Range is not defined, make request') as ErrorWithStatus
+        err.status = 416
+        return next()
+    }
+
+    const torrentFile = client.get(magnet) as Torrent
+    let file = <TorrentFile>{}
+    for (let i = 0; i < torrentFile.files.length; i++){
+        const currentTorrentPiece = torrentFile.files[i]
+        if (currentTorrentPiece.name === fileName) {
+            file = currentTorrentPiece
+        } 
+    }
+
+    const fileSize = file.length
+    const [startParsed, endParsed] = range.replace(/bytes=/,'').split('-')
+
+    const start = Number(startParsed)
+    const end = endParsed ? Number(endParsed) : fileSize - 1
+
+    const chunkSize = end - start + 1
+
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunkSize,
+        'Content-Type': 'video/mp4'
+    }
+
+    res.writeHead(206, headers)
+    
+    const streamPositions = {
+        start,
+        end
+    }
+
+    const stream = file.createReadStream(streamPositions)
+
+    stream.pipe(res)
+
+    stream.on('error', err => {
+        return next(err)
+    })
+})
+
 
 export default router
